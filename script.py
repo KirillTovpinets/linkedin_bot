bdcb4bb63d46c05e5dc18363d557f278857a9c47
Kiryl Toupinets <kiryl.toupinets@willolabs.com>
2024-12-10 18:04:56 -0600

    update cookie

--- script.py
+++ script.py
@@ -22,25 +22,75 @@ driver = webdriver.Chrome()
 # Open LinkedIn login page
 driver.get('https://www.linkedin.com/login')
 
-time.sleep(30)
+load_cookies(driver, "cookies.pkl")
 
-# Once logged in, save cookies to a file
-with open("cookies.pkl", "wb") as f:
-    pickle.dump(driver.get_cookies(), f)
+driver.get("https://www.linkedin.com/feed")
 
-print("Cookies saved successfully!")
 
-# Close the browser session
-driver.quit()
-
-# load_cookies(driver, "cookies.pkl")
-
-# # Enter credentials
+# # # Enter credentials
 # username = driver.find_element(By.ID, 'username')
 # username.send_keys('kirill.tovpinets@icloud.com')
 
 # password = driver.find_element(By.ID, 'password')
 # password.send_keys('x!VT2O0i#gN&')
 
-# Submit form
+# # Submit form
 # password.send_keys(Keys.RETURN)
+
+time.sleep(10)
+
+# # Once logged in, save cookies to a file
+# with open("cookies.pkl", "wb") as f:
+#     pickle.dump(driver.get_cookies(), f)
+
+# print("Cookies saved successfully!")
+
+search_box = driver.find_element(By.XPATH, "//input[@placeholder='Search']")
+try:
+    search_box.send_keys("Python Developer")
+    search_box.send_keys(Keys.RETURN)
+except Exception as e:
+    toggler = driver.find_element(By.CLASS_NAME, 'search-global-typeahead__collapsed-search-button')
+    toggler.click()
+
+    search_box = driver.find_element(By.XPATH, "//input[@placeholder='Search']")
+    search_box.send_keys("Python Developer")
+    search_box.send_keys(Keys.RETURN)
+
+time.sleep(5)
+easy_apply_filter = driver.find_element(By.XPATH, "//a[contains(., 'Easy apply')]")
+
+easy_apply_filter.click()
+
+time.sleep(10)
+
+jobs = driver.find_elements(By.XPATH, "//ul//li[@class='ember-view']")
+
+for job in jobs:
+    job.click()
+    time.sleep(5)
+    try:
+        apply_button = driver.find_element(By.XPATH, "//button[@class='jobs-apply-button']")
+        apply_button.click()
+        time.sleep(5)
+    except Exception as e:
+        print("Can't apply")
+
+    next_button = driver.find_element(By.XPATH, "//button[@data-easy-apply-next-button]")
+    next_button.click()
+    time.sleep(5)
+
+    next_button.click()
+    time.sleep(5)
+
+    next_button.click()
+    time.sleep(5)
+
+    preview_button = driver.find_element(By.XPATH, "//button[@data-live-test-easy-apply-review-button]")
+    preview_button.click()
+    time.sleep(5)
+
+    submit_button = driver.find_element(By.XPATH, "//button[@data-live-test-easy-apply-review-button]")
+    submit_button.click()
+    time.sleep(5)
+
