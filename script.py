3ae8df0d55ebac87ee3a5edaeeac5854a671ad55
toupinetswillo <ktoupinets@follett.com>
2025-02-26 15:56:47 -0600

    imporove scriopt

--- script.py
+++ script.py
@@ -24,6 +24,7 @@ driver.get('https://www.linkedin.com/login')
 
 load_cookies(driver, "cookies.pkl")
 
+print('Get To Linkedin')
 driver.get("https://www.linkedin.com/feed")
 
 
@@ -37,7 +38,7 @@ driver.get("https://www.linkedin.com/feed")
 # # Submit form
 # password.send_keys(Keys.RETURN)
 
-time.sleep(10)
+time.sleep(3)
 
 # # Once logged in, save cookies to a file
 # with open("cookies.pkl", "wb") as f:
@@ -46,6 +47,7 @@ time.sleep(10)
 # print("Cookies saved successfully!")
 
 search_box = driver.find_element(By.XPATH, "//input[@placeholder='Search']")
+print('Put keyword in search box...')
 try:
     search_box.send_keys("Python Developer")
     search_box.send_keys(Keys.RETURN)
@@ -58,39 +60,52 @@ except Exception as e:
     search_box.send_keys(Keys.RETURN)
 
 time.sleep(5)
+print('Filtering jobs by eash apply...')
 easy_apply_filter = driver.find_element(By.XPATH, "//a[contains(., 'Easy apply')]")
 
 easy_apply_filter.click()
 
-time.sleep(10)
+time.sleep(3)
 
-jobs = driver.find_elements(By.XPATH, "//ul//li[@class='ember-view']")
-
+print('Iterating over jobs...')
+jobs = driver.find_elements(By.XPATH, "//main[contains(@class,'scaffold-layout__list-detail')]//ul//li[contains(@class,'scaffold-layout__list-item')]")
 for job in jobs:
-    job.click()
-    time.sleep(5)
+  job_title = job.find_element(By.CSS_SELECTOR, "a.job-card-list__title--link")
+  print('clicking on job...', job_title.text)
+  job.click()
+  time.sleep(5)
+  job_details = driver.find_element(By.XPATH, "//div[@class='jobs-search__job-details--wrapper']")
+  try:
+      apply_button = job_details.find_element(By.XPATH, ".//button[contains(@class,'jobs-apply-button')]")
+      print('clicking on apply button...')
+      apply_button.click()
+      time.sleep(10)
+  except Exception as e:
+      print("Can't apply. Probably already applied")
+      continue
+
+  modal_dialog = driver.find_element(By.XPATH, "//div[@data-test-modal-id='easy-apply-modal']")
+  next_button = modal_dialog.find_element(By.XPATH, ".//button[@data-easy-apply-next-button]")
+
+  while(next_button):
     try:
-        apply_button = driver.find_element(By.XPATH, "//button[@class='jobs-apply-button']")
-        apply_button.click()
-        time.sleep(5)
+      next_button.click()
+      time.sleep(5)
+      next_button = modal_dialog.find_element(By.XPATH, ".//button[@data-easy-apply-next-button]")
     except Exception as e:
-        print("Can't apply")
+      print('No more next buttons')
+      break
 
-    next_button = driver.find_element(By.XPATH, "//button[@data-easy-apply-next-button]")
-    next_button.click()
-    time.sleep(5)
+  preview_button = modal_dialog.find_element(By.XPATH, ".//button[@data-live-test-easy-apply-review-button]")
+  preview_button.click()
+  time.sleep(5)
 
-    next_button.click()
-    time.sleep(5)
+  submit_button = modal_dialog.find_element(By.XPATH, ".//button[@data-live-test-easy-apply-submit-button]")
+  submit_button.click()
+  time.sleep(5)
 
-    next_button.click()
-    time.sleep(5)
-
-    preview_button = driver.find_element(By.XPATH, "//button[@data-live-test-easy-apply-review-button]")
-    preview_button.click()
-    time.sleep(5)
-
-    submit_button = driver.find_element(By.XPATH, "//button[@data-live-test-easy-apply-review-button]")
-    submit_button.click()
-    time.sleep(5)
+  success_dialog =  driver.find_element(By.XPATH, "//div[@aria-labelledby='post-apply-modal']")
+  close_button = success_dialog.find_element(By.XPATH, ".//button[@data-test-modal-close-btn]")
+  close_button.click()
+  time.sleep(5)
 
